fe50611ae763c8754d70e690eb10fd16bd8c79cd
Paul-Emmanuel Chouc <pechouc@gmail.com>
2024-01-22 10:21:21 +0100

    Cleaning the script that governs package installation

--- setup.py
+++ setup.py
@@ -5,12 +5,11 @@ with open('requirements.txt') as f:
     content = f.readlines()
 requirements = [x.strip() for x in content if 'git+' not in x]
 
-setup(name='dst_incidence',
-      version="1.0",
-      description="Project Description",
-      packages=find_packages(),
-      test_suite = 'tests',
-      # include_package_data: to install data from MANIFEST.in
-      include_package_data=True,
-      scripts=['scripts/dst_incidence-run'],
-      zip_safe=False)
+setup(
+    name='dst_incidence',
+    version="1.0",
+    description="Analysing the incidence of Digital Service Taxes (DSTs).",
+    packages=find_packages(),
+    test_suite='tests',
+    zip_safe=False
+)
